b4642b8967dd282b92a14e9177aae103db1a2db5
moholpranav <moholpranav98@gmail.com>
2024-10-16 16:49:24 +0530

    Add files via upload

---
+++ StudentManagmentSys/src/main/java/com/Project/StudentManagmentSys/StudentManagmentSysApplication.java
@@ -0,0 +1,13 @@
+package com.Project.StudentManagmentSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagmentSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagmentSysApplication.class, args);
+	}
+
+}
